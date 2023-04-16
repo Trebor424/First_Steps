659b3cc5c9decd83a6909af3eb14b0e518f40f00
Robert Pawelski <robert.pawelski97@gmail.com>
2023-04-16 16:47:35 +0200

    MaxValue

---
+++ 1MaxValue.py
@@ -0,0 +1,8 @@
+numbers = [5, 1, 0, 1, "1", -1, 5, -1]
+print(numbers)
+I_numbers= [int(i) for i in numbers]
+print(f"Max Value is = {max(I_numbers)}")
+print(f"Min Value is = {min(I_numbers)}")
+
+
+
